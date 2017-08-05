8cff512f17e9ded8edd6bc2dc3e9b10f8291a7a0
Kirka <kirka121@gmail.com>
2017-08-05 08:22:37 -0400

    started gathering

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,9 +2,10 @@ class DashboardsController < ApplicationController
 	before_action :authenticate_user!
 	before_action :set_s3_direct_post, only: [:show]
 
-
 	def show
-		@record = Record.new()
+		@record = Record.new
+		@records = current_user.records
+    @react_props = { records: @records, user: current_user, s3_url: @s3_direct_post.url, s3_fields: @s3_direct_post.fields }
 	end
 
 	private
